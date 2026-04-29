ad1f1a144eefa424b71c56f7af54a6ece236f637
Lucas Viana dos Santos <l.lucasantos2003@gmail.com>
2026-04-28 20:49:46 -0400

    Começo

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
+#include "main.h"
 #include <stdio.h>
 #include <string.h>
 
+Nodo raiz = (Nodo){.esq = NULL,
+                   .dir = NULL,
+                   .centro = NULL,
+                   .pai = NULL,
+                   .h = 0,
+                   .is_pasta = false};
+
+Nodo *corrente = &raiz;
+// Utils
 void copiarStr(char dest[], char orig[], int ini, int fim) {
   int i = 0, tam = strlen(orig);
   while (i + ini < tam && i < fim - ini && orig[i + ini] != '\0' &&
@@ -11,12 +21,97 @@ void copiarStr(char dest[], char orig[], int ini, int fim) {
   dest[i] = '\0';
 }
 
-void obter_comando(char cmd[]) {
-  char str[14];
+// Arvore
+Nodo *criarNodo(char nome[], bool pasta) {
+  Nodo *p = (Nodo *)malloc(sizeof(Nodo));
+
+  copiarStr(p->nome, nome, 0, NOME_ARQUIVO_TAM);
+
+  p->is_pasta = pasta;
+  p->centro = NULL;
+  p->esq = NULL;
+  p->dir = NULL;
+  p->pai = NULL;
+  p->h = 0;
+  return p;
+}
+
+int fb(Nodo *p) { // fun��o de balanceamento
+                  // completar
+}
+
+Nodo *rotDir(Nodo *raiz) {
+  // completar
+}
+
+Nodo *rotEsq(Nodo *raiz) {
+  // completar
+}
+
+Nodo *rotEsqDir(Nodo *raiz) {
+  // completar
+}
+
+Nodo *rotDirEsq(Nodo *raiz) {
+  // completar
+}
+
+Nodo *balancear(Nodo *raiz) {
+  // completar
+}
+
+void listar(Nodo *p) {
+  int i;
+  if (p) {
+    listar(p->dir);
+    printf("%s", p->nome);
+
+    if (p->is_pasta)
+      putc('-', stdout);
+
+    putc('\n', stdout);
+
+    listar(p->esq);
+  }
+}
+
+Nodo *inserirNodo(Nodo *raiz, char nome[], bool is_pasta) {
+  if (!raiz) {
+    raiz = criarNodo(nome, is_pasta);
+  } else {
+    if (strcmp(nome, raiz->nome) <= 0) {
+      raiz->esq = inserirNodo(raiz->esq, nome, is_pasta);
+      raiz->esq->pai = raiz;
+    } else {
+      raiz->dir = inserirNodo(raiz->dir, nome, is_pasta);
+      raiz->dir->pai = raiz;
+    }
+  }
+
+  return raiz;
+}
+
+Nodo *buscar(Nodo *raiz, char nome[]) {
+  if (raiz) {
+    if (strcmp(nome, raiz->nome) == 0) {
+      return raiz;
+    } else if (strcmp(nome, raiz->nome) < 0) {
+      return buscar(raiz->esq, nome);
+    } else
+      return buscar(raiz->dir, nome);
+  } else
+    return NULL;
+}
+
+Nodo *excluir(Nodo *raiz, int v) {}
+
+int obter_comando(char comando[], char argumento[]) {
+  char str[NOME_ARQUIVO_TAM + 3];
   fflush(stdin);
-  fgets(str, 14, stdin);
-  copiarStr(cmd, str, 0, 2);
-  copiarStr(cmd, str, 0, 2);
+  fgets(str, NOME_ARQUIVO_TAM + 3, stdin);
+
+  copiarStr(comando, str, 0, 2);
+  copiarStr(argumento, str, 2, NOME_ARQUIVO_TAM + 3);
 }
 
 void mostrar_caminho() {};
@@ -24,9 +119,25 @@ void mostrar_caminho() {};
 int main() {
   mostrar_caminho();
 
-  char cmd[3];
+  char cmd[3], argt[NOME_ARQUIVO_TAM];
   do {
-    obter_comando(cmd);
+    int a = obter_comando(cmd, argt);
+
+    if (strcmp(cmd, "ls") == 0) {
+      listar(corrente);
+    } else if (strcmp(cmd, "ma") == 0) {
+      inserirNodo(corrente, argt, false);
+    } else if (strcmp(cmd, "mp") == 0) {
+      inserirNodo(corrente, argt, true);
+    } else if (strcmp(cmd, "cd") == 0) {
+      Nodo *pasta = buscar(corrente, argt);
+
+      if (pasta && pasta->is_pasta)
+        corrente = pasta;
+
+    } else if (strcmp(cmd, "rm") == 0) {
+      // TODO
+    }
   } while (strcmp(cmd, "ex") != 0);
   return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NOME_ARQUIVO_TAM 32
+
+typedef struct nodo {
+  char nome[NOME_ARQUIVO_TAM];
+  bool is_pasta;
+  struct nodo *esq;
+  struct nodo *dir;
+  struct nodo *centro; // EXCLUSIVO PARA DIRETORIOS
+  struct nodo *pai;
+  int h;
+} Nodo;
+
+Nodo *criarNodo(char nome[], bool is_pasta);
+Nodo *buscar(Nodo *raiz, char nome[]);
+void listar(Nodo *p);
+Nodo *inserirNodo(Nodo *raiz, char nome[], bool is_pasta);
+Nodo *excluir(Nodo *raiz, int v);
+int fb(Nodo *p);
+Nodo *rotEsq(Nodo *raiz);
+Nodo *rotDir(Nodo *raiz);
+Nodo *rotEsqDir(Nodo *raiz);
+Nodo *rotDirEsq(Nodo *raiz);
+Nodo *balancear(Nodo *raiz);
